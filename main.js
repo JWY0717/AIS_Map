fe57fb837a5079c8abf19925788f0eb20cbfd3bf
Bogol-E <skylark@skku.edu>
2023-05-22 06:40:55 +0900

    22

--- main.js
+++ main.js
@@ -27,22 +27,32 @@ const map = new Map({
   }),
 });
 
-const goem = [14363620.688750563, 4174472.488498304]; // 마커의 초기 위치 좌표
-const sog = 10; // 노트 단위의 속도
-const cog = 140; // AIS 신호에서 수신된 방향 정보
-const marker = new Marker(goem, cog, map); // Marker 인스턴스 생성
+const markerData = [
+  { goem: [14363620.688750563, 4174472.488498304], sog: 20, cog: 140 },
+  { goem: [14363320.688750563, 4174772.488498304], sog: 10, cog: 20 },
+  { goem: [14363020.688750563, 4174072.488498304], sog: 15, cog: 60 },
+  { goem: [14364120.688750563, 4173772.488498304], sog: 10, cog: 10 },
+  { goem: [14364320.688750563, 4172772.488498304], sog: 40, cog: 340 },
+  { goem: [14366620.688750563, 4172772.488498304], sog: 50, cog: 290 },
+];
 
-map.addLayer(
-  new VectorLayer({
-    source: new VectorSource({
-      features: [marker.getFeature()], // marker의 feature를 사용하여 VectorSource 생성
-    }),
-  })
-);
+const markers = markerData.map(data => new Marker(data.goem, data.cog, map));
 
-function animateMarker() {
-  marker.updatePosition(sog, cog, map);
-  requestAnimationFrame(animateMarker);
+markers.forEach((marker, index) => {
+  map.addLayer(
+    new VectorLayer({
+      source: new VectorSource({
+        features: [marker.getFeature()],
+      }),
+    })
+  );
+  marker.sog = markerData[index].sog; 
+  marker.cog = markerData[index].cog; 
+});
+
+function animateMarkers() {
+  markers.forEach(marker => marker.updatePosition(marker.sog, marker.cog, map));
+  requestAnimationFrame(animateMarkers);
 }
 
-animateMarker();
+animateMarkers();
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // 상대 경로를 위해 './'로 설정합니다.
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInclude: ['**/*.svg'],
+  },
+});

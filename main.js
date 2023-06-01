b10a13fd3adc26e3c993bd0098ce8e8fa9dfd521
Bogol-E <skylark@skku.edu>
2023-06-01 14:04:42 +0900

    www

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ const debouncedEventHandler = debounce(function (event) {
 map.getView().on('change:resolution', debouncedEventHandler);
 
 
-const AIS_SERVER = "ws://192.168.219.107:9001"
+const AIS_SERVER = "ws://localhost:9001"
 const socket = new WebSocket(AIS_SERVER);
 socket.binaryType = "arraybuffer";
 socket.onopen = function () {

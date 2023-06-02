8504b72c7b60b1921e9cc3ccb7655f6d029457b6
Bogol-E <skylark@skku.edu>
2023-06-02 12:49:11 +0900

    22

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
   base: './', // 상대 경로를 위해 './'로 설정합니다.
@@ -10,7 +7,4 @@ export default defineConfig({
     assetsDir: 'assets',
     assetsInclude: ['**/*.svg'],
   },
-  plugins: [
-    commonjs(),
-  ],
 });

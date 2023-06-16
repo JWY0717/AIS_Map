24d6de4b3ab644021e95a706771b418e59cedba3
내 이름 <122770858+JWY0717@users.noreply.github.com>
2023-06-16 11:22:24 +0900

    222

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 import debounce from 'lodash/debounce';
 import Marker from './src/Marker';
 import aisDecoder from './src/aisDecoder'
+import { transform } from 'ol/proj';
 
 const extent = [13967488.396764, 3840850.295080, 14482255.536724, 4668126.023685];
 const vectorSource = new VectorSource();
@@ -49,34 +50,56 @@ markers.set(1, new Marker(
 
 listbox.innerHTML += '<p class="highlight">[ 소실 신호 목록 ]</p>';
 
-(async function initializeShips() {
-  try {
-    const response = await axios.get('http://58.78.120.69/aisdata');
-    const ships = response.data;
-    const now = Date.now();
-    for (const ais of ships) {
-      const elapsed = now - ais.time;
-      if (elapsed > (1000*60*5)) {
-        listbox.innerHTML += (`<p> ${~~((elapsed)/60/1000)}분간 소실 &nbsp;&nbsp;&nbsp;&nbsp; ${ais.shipName || "UnKnown"} [${ais.mmsi}]</p>`);
-      }
-      markers.set(ais.mmsi, new Marker(
-        ais.shipname, ais.shiptype, ais.trueheading, ais.cog, ais.sog,
-        ais.posx, ais.posy, ais.time, map, vectorSource));
-    }
-    checkLost();
-  } catch (error) {
-    console.error(error);
-  }
-})();
+// (async function initializeShips() {
+//   try {
+//     const response = await axios.get('http://58.78.120.69/aisdata');
+//     const ships = response.data;
+//     const now = Date.now();
+//     for (const ais of ships) {
+//       const elapsed = now - ais.time;
+//       if (elapsed > (1000*60*5)) {
+//         listbox.innerHTML += (`<p> ${~~((elapsed)/60/1000)}분간 소실 &nbsp;&nbsp;&nbsp;&nbsp; ${ais.shipName || "UnKnown"} [${ais.mmsi}]</p>`);
+//       }
+//       markers.set(ais.mmsi, new Marker(
+//         ais.shipname, ais.shiptype, ais.trueheading, ais.cog, ais.sog,
+//         ais.posx, ais.posy, ais.time, map, vectorSource));
+//     }
+//     checkLost();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
 
 function checkLost(){
   let nowTime = Date.now();
   const zoomLevel = map.getView().getZoom();
   for (const [_, marker] of markers) {
     if (nowTime-marker.time>300000) {
+
       marker.lost = true;
       marker.changeLost(zoomLevel);
-      
+      const [latitude, longitude] = transform([marker.posX, marker.posY], 'EPSG:3857', 'EPSG:4326')
+
+      (async function predictAis() {
+        try {
+          const data = {
+            mmsi: marker.mmsi,
+            ship_type: marker.ship_type,
+            latitude : latitude,
+            longitude : longitude,
+            cog : marker.cog,
+            sog : marker.sog
+          }
+          const response = await axios.post('http://127.0.0.1:5000/api05/predict', data);
+
+          const res = response.data;
+          marker.updateMarker(res.predicted_cog, res.predicted_cog, res.predicted_sog, marker.time + 60000)
+
+          console.log(res);
+        } catch (error) {
+          console.error(error);
+        }
+      })();
     }
   }
 }
